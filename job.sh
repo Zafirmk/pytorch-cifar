73aa8a6a2ff9b945cc9c5c993821cf22dfb055a1
zafirmk <zafirmk0@gmail.com>
2024-01-16 16:52:53 -0500

    update job.sh

---
+++ job.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -time=00:10:00
+#SBATCH --mail-user=z_khalid@live.concordia.ca
+#SBATCH --mail-type=ALL
+#SBATCH --account=rrg-eugenium
+#SBATCH --mem=32000M
+#SBATCH --gpus-per-node=1
+#SBATCH --cpus-per-task=2
+
+cd ~/projects/rrg-eugenium/zafirmk/pytorch-cifar
+module load
+module python/3.9
+source ~/cifar10venv/bin/activate
+python main.py
