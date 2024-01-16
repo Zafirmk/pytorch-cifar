6a9775568606f99df94a0786418c287fc183d94e
Zafirmk <zafirmk0@gmail.com>
2024-01-17 03:53:17 +0400

    fixes

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 import torch.backends.cudnn as cudnn
-# import wandb
+import wandb
 
 import torchvision
 import torchvision.transforms as transforms
@@ -14,10 +14,10 @@ import argparse
 
 from models import *
 
-# wandb.init(project = "CIFAR10")
+wandb.init(project = "CIFAR10", mode="offline")
 
-# WANDB_API_KEY = os.environ.get("WANDB_API_KEY")
-# WANDB_USERNAME = os.environ.get("WANDB_USERNAME")
+WANDB_API_KEY = os.environ.get("WANDB_API_KEY")
+WANDB_USERNAME = os.environ.get("WANDB_USERNAME")
 
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
 parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
@@ -43,13 +43,21 @@ transform_test = transforms.Compose([
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
-trainset = torchvision.datasets.CIFAR10(
-    root='./data', train=True, download=True, transform=transform_train)
+trainset = torchvision.datasets.CIFAR10(root='/home/zafirmk/scratch/cifar', 
+                   train=True, 
+                   download=False,
+                   transform=transform_train)
 trainloader = torch.utils.data.DataLoader(
     trainset, batch_size=128, shuffle=True, num_workers=2)
 
 testset = torchvision.datasets.CIFAR10(
     root='./data', train=False, download=True, transform=transform_test)
+testset = torchvision.datasets.CIFAR10(root='/home/zafirmk/scratch/cifar', 
+                   train=False, 
+                   download=False,
+                   transform=transform_test)
+
+
 testloader = torch.utils.data.DataLoader(
     testset, batch_size=100, shuffle=False, num_workers=2)
 
