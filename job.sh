04403b397a56e7945483beca4156577f51df6b40
Zafirmk <zafirmk0@gmail.com>
2024-01-17 08:42:23 +0400

    fix venv path

--- job.sh
+++ job.sh
@@ -10,5 +10,5 @@
 cd ~/projects/rrg-eugenium/zafirmk/pytorch-cifar
 module load
 module python/3.9
-source ~/cifar10venv/bin/activate
+source ~/projects/rrg-eugenium/zafirmk/pytorch-cifar/cifar10venv
 python main.py
