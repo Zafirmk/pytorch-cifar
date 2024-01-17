c20f5b3fc7c16e63ad1c9603cfe15f0d68ceb784
Zafirmk <zafirmk0@gmail.com>
2024-01-17 12:19:04 +0400

    fix

--- main.py
+++ main.py
@@ -50,8 +50,6 @@ trainset = torchvision.datasets.CIFAR10(root='/home/zafirmk/scratch/cifar',
 trainloader = torch.utils.data.DataLoader(
     trainset, batch_size=128, shuffle=True, num_workers=2)
 
-testset = torchvision.datasets.CIFAR10(
-    root='./data', train=False, download=True, transform=transform_test)
 testset = torchvision.datasets.CIFAR10(root='/home/zafirmk/scratch/cifar', 
                    train=False, 
                    download=False,
